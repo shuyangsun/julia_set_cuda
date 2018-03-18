226dbe63a5aa7dcbcde54bff9e61648ed0ff028a
Shuyang Sun <sunbuffett@gmail.com>
2018-03-18 16:18:48 -0400

    Add helper.hpp

---
+++ src/helper.hpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+#include "cuda_runtime.h"
+
+static void HandleCUDAError(cudaError_t const err, const char *file, int line) {
+    if (err != cudaSuccess) {
+        std::string err_msg{ cudaGetErrorString(err) };
+        err_msg += " in ";
+        err_msg += std::string{ file };
+        err_msg += " at line ";
+        err_msg += std::to_string(line);
+        err_msg += "\n";
+        std::cerr << err_msg << std::endl;
+        throw std::runtime_error(err_msg);
+    }
+}
+
+#define HANDLE_CUDA_ERROR( err ) (HandleCUDAError( err, __FILE__, __LINE__ ))
